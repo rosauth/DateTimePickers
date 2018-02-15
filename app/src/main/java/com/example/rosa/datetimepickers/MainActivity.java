b1cf998783a6158d5dd465a9e9fafa361bcfab33
Rosa <rosagaghana@gmail.com>
2018-02-15 16:18:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rosa/datetimepickers/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.rosa.datetimepickers;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePickerDialog(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.date_picker));
+    }
+
+    public void showTimePickerDialog(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.time_picker));
+    }
+}
